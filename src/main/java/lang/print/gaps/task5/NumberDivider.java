1edea70b6222d5d10b3c9da56c3b3dbd89f859b3
zanzibra <talbergaerika@gmail.com>
2022-10-18 20:19:24 +0300

    1st try

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        double result = toBeDivided / 5;
+        System.out.println(result);
     }
 }
